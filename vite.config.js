bc442b52206275da255f72e788f5657c42521b71
Stephen Hawes <sphawes@gmail.com>
2025-07-26 09:22:12 -0400

    trying to get help.html to be included

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 export default {
     base: './',
     optimizeDeps: { exclude: ["fsevents"] },
-    publicDir: 'public'
+    publicDir: 'public',
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            help: resolve(__dirname, 'help.html')
+          }
+        }
+      }
 }
